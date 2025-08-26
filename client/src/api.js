1a4cf773f31641fca53f21915ed12dd6f58ffe45
Siddharth <100620158+isidrohan@users.noreply.github.com>
2025-08-27 01:36:29 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
+
   // baseURL: "https://ai-diet-backend-2ls2.onrender.com/api",
   baseURL:  `${import.meta.env.VITE_BASE_URL}/api`,
-  // baseURL:  "http://localhost:5000/api",
 });
 
 export default API;
