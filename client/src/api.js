573cb99204460ce48a8ee6e9c02a17894bfadb0d
Siddharth Rohan <sidrohanme2@gmail.com>
2025-08-27 00:44:06 +0530

    fixing render showing undefine

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-diet-backend-2ls2.onrender.com/api",
+  // baseURL: "https://ai-diet-backend-2ls2.onrender.com/api",
+  baseURL:  `${import.meta.env.VITE_BASE_URL}/api`,
 });
 
 export default API;
